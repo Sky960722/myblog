cc65f018e43db164813c5ccd530db6002d954bff
long <1448131306@qq.com>
2022-05-21 22:58:43 +0800

    大概成型了

--- src/main/java/com/sry/springboot/myblog/config/Constants.java
+++ src/main/java/com/sry/springboot/myblog/config/Constants.java
@@ -1,6 +1,7 @@
 package com.sry.springboot.myblog.config;
 
 public class Constants {
-    public final static String FILE_UPLOAD_DIC = "D:\\myBlogTest\\";//上线改成linux
+    //public final static String FILE_UPLOAD_DIC = "D:\\myBlogTest\\";//上线改成linux
 
+    public final static String FILE_UPLOAD_DIC = "/root/myblog/upload/";//上线改成linux
 }
